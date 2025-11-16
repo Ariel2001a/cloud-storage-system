0add64e3ea94fe054de66c477007897a9e0947bd
dvirtabib <dvirtb@gmail.com>
2025-11-16 20:34:51 +0200

    Get command - test 4

---
+++ getcommand.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+#ifndef GETCOMMAND_H
+#define GETCOMMAND_H
+
+extern std::map<std::string, std::string> envMap;
+
+char* find_environment_variable(std::string file_name);
+
+std::string get_file_content(std::string file_name);
+
+std::string set_file_content(std::string file_name);
+
+std::string decompress(std::string compress_content);
+
+std::string local_variable(std::string file_name);
+
+#endif
---
+++ test.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+#include <map>
+#include "getcommand.h"
+
+TEST(FindEnvironmentVariableTest, HandlesExistingAndNonExistingVars) {
+    EXPECT_EQ(find_environment_variable("abc"), nullptr);
+    envMap["CONFIG_FILE"] = "config.txt";
+    EXPECT_STREQ(find_environment_variable("CONFIG_FILE"), "config.txt");
+}
+
+TEST(GetFileContentTest, HandlesExistingAndNonExistingStrings) {
+    EXPECT_EQ(get_file_content("abc"), "");
+    envMap["CONFIG_FILE"] = "config.txt";
+    EXPECT_STREQ(get_file_content("CONFIG_FILE").c_str(), "Hello World");
+}
+
+TEST(DecompressTest, HandlesDecompressStrings) {
+    EXPECT_STREQ(decompress("abc").c_str(), "");
+    EXPECT_STREQ(decompress("a12b3c1").c_str(), "aaaaaaaaaaaabbbc");
+    EXPECT_STREQ(decompress("b3a12c1").c_str(), "bbbaaaaaaaaaaaac");
+    EXPECT_STREQ(decompress("b3 a12 c1").c_str(), "bbb aaaaaaaaaaaa c");
+}
+
+TEST(LocalVariableTest, HandlesLocalVariableRetrieval) {
+    envMap["CONFIG_FILE"] = "config.txt";
+    EXPECT_STREQ(local_variable("CONFIG_FILE").c_str(), decompress("H1e1l2o1 W1o1r1l1d1").c_str());
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
