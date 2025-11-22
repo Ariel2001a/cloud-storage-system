4b5802ae05b81578c733ae409d293468fb48a7cd
dvirtabib <dvirtb@gmail.com>
2025-11-22 21:25:57 +0200

    mainUpdate

---
+++ main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+#include "ICommand.h"
+#include "CommandManager.h"
+#include "GetCommand.h"
+#include "Compressor.h"
+
+using namespace std;
+
+int main() {
+    
+    Compressor comp;
+    
+    CommandManager manager;
+  
+    manager.registerCommand(new GetCommand());
+    
+
+    while (true) {
+        string line;
+        
+        getline(cin, line);
+      
+
+        if (line.empty()) continue;
+       
+
+        string cmdName = parseCmd(line);
+        vector<string> args = parseArgs(line);
+
+        if (!validateInput(cmdName, args)) {
+            continue;
+        }
+
+        manager.runCommand(cmdName, args);
+    }
+    return 0;
+}
+
+string parseCmd(const string& line) {
+    istringstream iss(line); 
+    string cmd;
+    iss >> cmd;                    
+    return cmd;                    
+}
+
+vector<string> parseArgs(const string& line) {
+    istringstream iss(line);
+    string cmd;
+    iss >> cmd;  
+    vector<string> args;
+    string arg;
+    while (iss >> arg) {
+        args.push_back(arg);
+    }
+    return args;
+}
+
+bool validateInput(const string& cmdName, const vector<string>& args) {
+
+    string fileName = args[0];
+
+    if (cmdName.empty()) {
+        return false;
+    }
+
+    if (args.empty()) {
+        return false;
+    }
+
+    if (args[0].empty()) {
+        return false;
+    }
+
+    if (isWhitespaceOnly(fileName)){
+        return false;
+    }
+
+    return true;
+}
+
+bool isWhitespaceOnly(const string& s) {
+    for (char c : s) {
+        if (c != ' ' && c != '\t' && c != '\n' && c != '\r') {
+            return false; 
+        }
+    }
+    return true;
+}
