4ba1c20cd4b5a243f0be5c436325bde426d72d04
dvirtabib <dvirtb@gmail.com>
2025-11-23 14:03:33 +0200

    commit

--- GetCommand.cpp
+++ GetCommand.cpp
@@ -1,52 +1,48 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> PASP-26-get-the-content-from-the-file
 #include <iostream>
 #include <string>
 #include <map>
 #include <fstream>
-using namespace std;
+#include <sstream>
+
 #include "ICommand.h"
 #include "Compressor.h"
 #include "GetCommand.h"
-#include <filesystem>
-#include <sstream>
 
-map<string, string> m;
-GetCommand::GetCommand(const string& name_file){
+std::map<std::string, std::string> m;
+
+GetCommand::GetCommand(const std::string& name_file){
     fileName = name_file;
     const char* folder = getenv("EX1_DIR");
     if (!folder){
          return;
     }
 
-    string fullPath = string(folder) + "/" + fileName;
+    std::string fullPath = std::string(folder) + "/" + fileName;
     std::ofstream file(fullPath);
     if (!file) {
         return;
     }
 
-    string content = "1H1e2l1o 1W1o1r1l1d";
+    std::string content = "1H1e2l1o 1W1o1r1l1d";
     file << content;
     file.close();
 }
 
-string GetCommand::findEnvironmentVariable() {
+std::string GetCommand::findEnvironmentVariable() {
     const char* folder = getenv("EX1_DIR");
     if (!folder) return "";
-    return string(folder) + "/" + fileName;
+    return std::string(folder) + "/" + fileName;
 }
 
-string GetCommand::getContentFile(const string& environment_variable_path) {
-    string compress_content;
-    ifstream file(environment_variable_path);
+std::string GetCommand::getContentFile(const std::string& environment_variable_path) {
+    std::string compress_content;
+    std::ifstream file(environment_variable_path);
             
     if (!file) {
         return "";
     }
 
-    string line;
+    std::string line;
     while (getline(file, line)) {
          compress_content += line;
     }
@@ -55,14 +51,14 @@ string GetCommand::getContentFile(const string& environment_variable_path) {
     return compress_content;
 } 
 
-void GetCommand::run(const vector<string>& args) {
+void GetCommand::run(const std::vector<std::string>& args) {
     if (args.size() != 1) {
         return;
     }
 
     std::string full = args[0];
     std::istringstream iss(full);
-    string cmd, file;
+    std::string cmd, file;
 
     if (!(iss >> cmd >> file)) {
         return;
@@ -73,20 +69,20 @@ void GetCommand::run(const vector<string>& args) {
         return;
     }
     GetCommand getcmd(file);
-    string environment_variable_path = getcmd.findEnvironmentVariable();
+    std::string environment_variable_path = getcmd.findEnvironmentVariable();
     if (environment_variable_path.empty()) {
         return;
     }
 
-    string compress_content = getcmd.getContentFile(environment_variable_path);
+    std::string compress_content = getcmd.getContentFile(environment_variable_path);
     if (compress_content.empty()) {
         return;
     }
 
-    string decompressed_content = Compressor::decompress(compress_content);
+    std::string decompressed_content = Compressor::decompress(compress_content);
     if (decompressed_content.empty()) {
         return;
     }
 
-    cout << decompressed_content << endl;
+    std::cout << decompressed_content << std::endl;
 }
