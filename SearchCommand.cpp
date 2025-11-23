04b0f70c089ee70f06427a809fc31787ecfe0465
eylon1303 <Eylonhakak13@gmail.com>
2025-11-23 19:28:47 +0200

    searching function

---
+++ SearchCommand.cpp
@@ -0,0 +1,43 @@
+#include "SearchCommand.h"
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+//Constructor
+SearchCommand::SearchCommand(Compressor* compPtr, const std::string& folderPath)
+    : ICommand("search"), comp(compPtr), folder(folderPath) {}
+
+
+//Run command (CommandManager)
+void SearchCommand::run(const std::vector<std::string>& args) {
+    if (args.empty()) return;
+
+    std::vector<std::string> results = search(args[0]);
+    
+}
+
+
+ 
+//returns vector of matching filenames
+std::vector<std::string> SearchCommand::search(const std::string& query) {
+    std::vector<std::string> results;
+
+    namespace fs = std::filesystem;
+    fs::path dir(folder);
+
+    for (const auto& entry : fs::directory_iterator(dir)) {
+        std::ifstream file(entry.path());
+
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        std::string fileContents = buffer.str();
+
+        std::string temp = comp->decompress(fileContents);
+
+        if (temp.find(query) != std::string::npos)
+            results.push_back(entry.path().filename().string());
+    }
+
+    return results;
+}
---
+++ SearchCommand.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "ICommand.h"
+#include "Compressor.h"
+#include <vector>
+#include <string>
+
+class SearchCommand : public ICommand {
+private:
+    std::string folder;
+    Compressor* comp;
+
+public:
+    // Constructor
+    SearchCommand(Compressor* compPtr, const std::string& folderPath);
+
+    // Overrides ICommand run
+    void run(const std::vector<std::string>& args) override;
+
+    // For tests: returns matching filenames
+    std::vector<std::string> search(const std::string& query);
+};
