08b5eb551844dd5e941df89e02ee8843dc2525ad
ArielG <ariel789987@gmail.com>
2025-11-24 18:24:33 +0200

    commit to main with updated version

--- test.cpp
+++ test.cpp
@@ -103,6 +103,105 @@ TEST(CompressorTest, Compress_MixedCharacters) {
 
 
 
+
+ void CreateTestFiles(const std::string& folder) {
+    Compressor comp;
+
+    std::ofstream(folder + "/First.txt") << comp.compress("this is the first file");
+    std::ofstream(folder + "/Second.txt") << comp.compress("now im saving the second test file");
+    std::ofstream(folder + "/Third.txt") << comp.compress("and this the last test file");
+}
+
+
+
+std::string Get_Folder()
+{
+    const char* env = std::getenv("EX1_DIR"); 
+    std::string folder;
+        folder = env; 
+
+    return folder;
+}
+
+
+
+
+//helper function to check if the files exist without specific order
+bool contains(const std::vector<std::string>& vec, const std::string& value) {
+    for (const auto& s : vec) {
+        if (s == value) return true;
+    }
+    return false;
+}
+
+
+
+    TEST(SearchTests, SingleMatch_test)
+{
+    std::string folder = Get_Folder();
+    CreateTestFiles(folder);
+
+    Compressor comp;
+    
+
+    SearchCommand searchCmd(&comp, folder);
+    auto results = searchCmd.search("sec");
+   
+    ASSERT_EQ(results.size(), 1);
+    EXPECT_EQ(results[0], "Second.txt");
+}
+
+TEST(SearchTests, MultipleMatch_test)
+{
+    std::string folder = Get_Folder();
+    CreateTestFiles(folder);
+
+    Compressor comp;
+    
+
+
+    SearchCommand searchCmd(&comp, folder);
+    auto results = searchCmd.search("file");
+    
+
+    ASSERT_EQ(results.size(), 3);
+    EXPECT_TRUE(contains(results, "First.txt"));
+    EXPECT_TRUE(contains(results, "Second.txt"));
+    EXPECT_TRUE(contains(results, "Third.txt"));
+}
+
+TEST(SearchTests, NoMatch_test)
+{
+    std::string folder = Get_Folder();
+    CreateTestFiles(folder);
+
+    Compressor comp;
+    
+
+
+    SearchCommand searchCmd(&comp, folder);
+    auto results = searchCmd.search("fourth");
+   
+    ASSERT_EQ(results.size(), 0);
+}
+
+TEST(SearchTests, Space_test)
+{
+    std::string folder = Get_Folder();
+    CreateTestFiles(folder);
+
+    Compressor comp;
+    
+
+
+    SearchCommand searchCmd(&comp, folder);
+    auto results = searchCmd.search("el");  // appears in "e l" and "le", make sure neither count as a match.
+
+    ASSERT_EQ(results.size(), 0);
+}
+
+
+
 // --- GoogleTest main ---
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
