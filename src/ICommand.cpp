75cbfea1c8eba35fe57a5cb14c0273d7635658a9
eylon1303 <Eylonhakak13@gmail.com>
2025-12-08 05:04:32 +0200

    dockers

--- src/ICommand.cpp
+++ src/ICommand.cpp
@@ -7,7 +7,7 @@ using namespace std;
 // Get the full path of a file using the EX1_DIR environment variable
 // Returns empty string if the environment variable is not set
 string ICommand::GetFolderPath() {
-    const char* folder = getenv("EX1_DIR");  // retrieve environment variable
+    const char* folder = getenv("DIR");  // retrieve environment variable
     if (!folder) return "";                  // return empty if variable is not found
     return string(folder); 
 }
