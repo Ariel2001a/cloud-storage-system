12e30c7837d5848b7ae88b986fa0dee4c45bbca0
eylon1303 <Eylonhakak13@gmail.com>
2025-12-09 20:01:32 +0200

    refactor

--- src/ClientCpp/tcp_c.cpp
+++ src/ClientCpp/tcp_c.cpp
@@ -11,66 +11,72 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
+    // make sure we got IP and port
     if (argc != 3) {
-    std::cerr << "Usage: ./client <server_ip> <port>\n";
-    return 1;
-}
-
-    std::string server_ip = argv[1];      
-    int port = std::atoi(argv[2]); 
-
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("error creating socket");
+        cout << SERVER_ERROR << endl;
+        return 1;
     }
 
+    // read server details from args
+    std::string server_ip = argv[1];
+    int port = std::atoi(argv[2]);
+
+    // create a TCP socket
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        cout << SERVER_ERROR << endl;
+    }
+
+    // fill server address info
     struct sockaddr_in sin;
     memset(&sin, 0, sizeof(sin));
     sin.sin_family = AF_INET;
     sin.sin_addr.s_addr = inet_addr(server_ip.c_str());
     sin.sin_port = htons(port);
 
+    // try to connect to the server
     if (connect(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
-        perror("error connecting to server");
+        cout << SERVER_ERROR << endl;
     }
- while (true) {
-        
-        string line;
-        getline(cin, line);         // read full line from user
 
-        // send command to server
+    // main loop: read user input, send to server, get response
+    while (true) {
+
+        // get a full line from the user
+        string line;
+        getline(cin, line);
+
+        // send it to the server
         int sent_bytes = send(sock, line.c_str(), line.size(), 0);
         if (sent_bytes < 0) {
-            perror("send failed");
+            cout << SERVER_ERROR << endl;
             break;
         }
 
-
-
+        // buffer for server reply
         char buffer[4096];
         int expected_data_len = sizeof(buffer);
+
+        // wait for server response
         int read_bytes = recv(sock, buffer, expected_data_len, 0);
 
-    if (read_bytes == 0) {
+        // server closed connection
+        if (read_bytes == 0) {
             break;
-    // connection is closed
+        }
+        // error reading
+        else if (read_bytes < 0) {
+            cout << SERVER_ERROR << endl;
+            break;
+        }
+        // got data, print it
+        else {
+            cout << string(buffer, read_bytes) << endl;
+        }
     }
-    else if (read_bytes < 0) {
-            
-          perror("recv failed");
-          break;
-    // error
-    }
-    else {
-        cout << string(buffer, read_bytes) << endl;
 
-    }
-
-   
-}
-
- close(sock);
-
+    // close the socket
+    close(sock);
 
     return 0;
 }
