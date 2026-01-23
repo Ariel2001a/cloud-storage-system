b5352f8edf30a8752cbba6729019e6f73c08b82c
dvirtabib <dvirtb@gmail.com>
2026-01-23 14:44:30 +0200

    commit

--- src/server_database/routes/files.js
+++ src/server_database/routes/files.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const filesController = require('../controllers/files'); // files controller
-const isLoggedIn = require('../middleware/auth');       // JWT middleware
+const isLoggedIn = require('../middleware/auth');       // JWT  middleware
 
 
 // Routes for top-level files/folders
